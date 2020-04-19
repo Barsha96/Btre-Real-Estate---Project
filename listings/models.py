4a0280e175208988da361c19f8aa890e3718d953
Barsha Upadhyaya <barsha.upadhaya1996@gmail.com>
2020-04-19 18:14:20 +0545

    models listings and realtors migrated

--- listings/models.py
+++ listings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
-class Listing(models.model):
+class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     #Realtor needs to be brought in from the realtor model
     title = models.CharField(max_length=200) 
