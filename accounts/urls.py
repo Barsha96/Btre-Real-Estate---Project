9419776de0d0dbc49a18b1cebce1afd339984b82
Barsha Upadhyaya <barsha.upadhaya1996@gmail.com>
2020-04-26 12:30:08 +0545

    provided urls for account app to the templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,10 @@
 #note- an app doesnt come with urls.py so we had to create one. which was also done in all other apps except btre which is our project
+from django.urls import path
+from . import views
+#we are gonna tell te main url file that anything that has listings/ will look at this file
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
